df2ec537d1017ff37e6df328919b3b7c5835ecc4
Armstrong Subero <armstrongsubero@gmail.com>
2018-09-07 19:30:11 -0400

    Add files via upload

---
+++ 05_External_Interrupt.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=05_External_Interrupt.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/05_External_Interrupt.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=05externalinterrupt.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/05externalinterrupt.x.tar
